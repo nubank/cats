c0e53058404b37b86514f755bc795e3517088324
Nu Deployer (10.129.212.95-a-agent-release-lib) <tech@nubank.com.br>
2018-09-04 18:27:04 +0000

    Version 2.3.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/cats "2.3.1"
+(defproject nubank/cats "2.3.2-SNAPSHOT"
   :description "Category Theory abstractions for Clojure"
   :url         "https://github.com/funcool/cats"
   :license {:name "BSD (2 Clause)"
